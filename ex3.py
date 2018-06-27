bcabd8d90782d90aac3b7dfa8a8db5514a8bb7d2
dani3lgx <37733768+dani3lgx@users.noreply.github.com>
2018-06-27 12:30:40 +0300

    .

--- ex3.py
+++ ex3.py
@@ -3,30 +3,6 @@ import random
 
 ids = ["935885178", "203609177"]
 
-def neighbors_value_sum(state, row, col):
-    count=0.0
-    ghost_count=0.0
-    if state[row][col] in (11, 12, 13):
-        count += 1
-    if state[row+1][col] in (11, 12, 13):
-        count += 1
-    if state[row][col+1] in (11, 12, 13):
-        count += 1
-    if state[row-1][col] in (11, 12, 13):
-        count += 1
-    if state[row][col-1] in (11, 12, 13):
-        count += 1
-    if state[row + 1][col] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
-        ghost_count += 1
-    if state[row][col + 1] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
-        ghost_count += 1
-    if state[row - 1][col] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
-        ghost_count += 1
-    if state[row][col - 1] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
-        ghost_count += 1
-    ghost_factor=50*ghost_count
-    return (count - ghost_factor)
-
 
 
 def count_dots(state):
@@ -58,6 +34,38 @@ class PacmanController:
         self.steps = steps
         self.dot_sum = count_dots(state)
         self.pacman_row, self.pacman_col = find_pacman(state)
+        self.prev_accumulated_reward = 0
+        self.dot_type_check = 0
+        self.dot_value = {11: 1, 12: 1,13: 1}
+        self.dot_amount = {11: 0, 12: 0,13: 0}
+
+    def neighbors_value_sum(self,state, row, col):
+        count = 0.0
+        ghost_count = 0.0
+        if state[row][col] in (11, 12, 13):
+            count += self.dot_value[state[row][col]]
+        if state[row + 1][col] in (11, 12, 13):
+            count += self.dot_value[state[row+1][col]]
+        if state[row][col + 1] in (11, 12, 13):
+            count += self.dot_value[state[row][col+1]]
+        if state[row - 1][col] in (11, 12, 13):
+            count += self.dot_value[state[row-1][col]]
+        if state[row][col - 1] in (11, 12, 13):
+            count += self.dot_value[state[row][col-1]]
+        if state[row + 1][col] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
+            ghost_count += 1
+        if state[row][col + 1] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
+            ghost_count += 1
+        if state[row - 1][col] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
+            ghost_count += 1
+        if state[row][col - 1] in (53, 52, 51, 50, 43, 42, 41, 40, 33, 32, 31, 30, 23, 22, 21, 20):
+            ghost_count += 1
+        ghost_factor = 50 * ghost_count
+        if state[row][col] in (11, 12, 13):
+            field = state[row][col]
+        else:
+            field = 0
+        return (count - ghost_factor), field
 
 
     def actions(self, state):
@@ -84,7 +92,7 @@ class PacmanController:
 
 
     def choose_next_action(self, state, accumulated_reward):
-        """Choose next action for pacman controller given the current state.
+        """Choose next action for pacman controller g   iven the current state.
         Action should be returned in the format described previous parts of the project.
         This method MUST terminate within the specified timeout.
         """
@@ -94,22 +102,32 @@ class PacmanController:
         if not allowed_lst:
             return "reset"
         pacman_row, pacman_col = find_pacman(state)
+
+        if accumulated_reward is not None:
+            value = accumulated_reward -  self.prev_accumulated_reward
+            print(value)
+            if self.dot_type_check != 0:
+                self.dot_amount[self.dot_type_check] = self.dot_amount[self.dot_type_check] + 1
+                self.dot_value[self.dot_type_check]=self.dot_value[self.dot_type_check]+(value-self.dot_value[self.dot_type_check])/self.dot_amount[self.dot_type_check]
+                self.prev_accumulated_reward = accumulated_reward
+        print(self.dot_value)
         value = -200
         action = None
         for act in allowed_lst:
             if act == "R":
-                tmp_value = neighbors_value_sum(state,pacman_row,pacman_col + 1)
+                tmp_value, tmp_dot_type = self.neighbors_value_sum(state,pacman_row,pacman_col + 1)
 
             if act == "D":
-                tmp_value = neighbors_value_sum(state, pacman_row + 1, pacman_col)
+                tmp_value, tmp_dot_type = self.neighbors_value_sum(state, pacman_row + 1, pacman_col)
 
             if act == "L":
-                tmp_value = neighbors_value_sum(state, pacman_row, pacman_col - 1)
+                tmp_value, tmp_dot_type = self.neighbors_value_sum(state, pacman_row, pacman_col - 1)
 
             if act == "U":
-                tmp_value = neighbors_value_sum(state, pacman_row - 1, pacman_col)
+                tmp_value, tmp_dot_type = self.neighbors_value_sum(state, pacman_row - 1, pacman_col)
             if tmp_value>=value:
                 value = tmp_value
                 action = act
-
+                dot_type = tmp_dot_type
+        self.dot_type_check = dot_type
         return action
